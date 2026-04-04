a251ca3c295dd00aeee4188c8e54c4680ce67fea
jakobdubeau <jakobdubeau@gmail.com>
2026-04-04 02:00:40 -0400

    make button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
+"use client"
 import Link from "next/link"
+import { useState } from "react";
 
 export default function Header() {
 
@@ -12,6 +14,8 @@ export default function Header() {
       href: "/insert",
     },
   ]
+
+  const [role, setRole] = useState(false)
 
   return (
     <div className="flex justify-between items-baseline px-16 md:px-30 pt-8">
@@ -30,8 +34,11 @@ export default function Header() {
             ))}
         </div>
         <div className="flex-1 flex items-center justify-end">
-            <button className="px-8 py-2 bg-blue-300 text-white rounded-full text-2xl cursor-pointer">
-                Hi
+            <button
+                className="w-40 py-2 bg-blue-300 text-white rounded-full text-2xl cursor-pointer hover:bg-blue-400 duration-200 transition-all"
+                onClick={() => setRole(!role)}
+            >
+                {role ? "Employee" : "Customer"}
             </button>
         </div>
     </div>
