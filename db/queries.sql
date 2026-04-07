7c06ba3d61ad2bd5b4dca8f9f526d24956db493d
William Bickerton <wbick096@uottawa.ca>
2026-04-07 13:18:10 -0400

    adding first querry find all bookings for a user

--- db/queries.sql
+++ db/queries.sql
@@ -1,1 +1,15 @@
 -- queries
+
+--find all bookings for a given user
+SELECT 
+    b.booking_id,
+    c.name
+    h.hotel_id,
+    h.address
+    b.room_number,
+    b.start_date,
+    b.end_date
+FROM booking b
+JOIN customer c ON b.customer_ssn = c.ssn
+JOIN hotel h ON b.hotel_id = h.hotel_id
+ORDER BY b.start_date;
