a56ac373e6f0748a10e66d7fa5939b8dd17cd7b9
William Bickerton <wbick096@uottawa.ca>
2026-04-07 18:46:56 -0400

    queries completed

--- db/queries.sql
+++ db/queries.sql
@@ -2,14 +2,54 @@
 
 --find all bookings for a given user
 SELECT 
+    c.name AS customer_name,
     b.booking_id,
-    c.name
     h.hotel_id,
-    h.address
+    h.address AS hotel_address,
     b.room_number,
     b.start_date,
     b.end_date
 FROM booking b
 JOIN customer c ON b.customer_ssn = c.ssn
 JOIN hotel h ON b.hotel_id = h.hotel_id
+WHERE c.ssn = '111-22-3333'
 ORDER BY b.start_date;
+
+--find employees working in a hotel
+SELECT 
+    e.ssn,
+    e.full_name,
+    e.address,
+    e.roles
+FROM employee e
+WHERE e.hotel_id = 1;   -- replace with the actual hotel_id
+
+
+--aggregation
+--counts rooms for a given hotel
+SELECT 
+    h.hotel_id,
+    h.address AS hotel_address,
+    COUNT(r.number) AS total_rooms
+FROM hotel h
+LEFT JOIN room r ON h.hotel_id = r.hotel_id
+WHERE h.hotel_id = 1
+GROUP BY h.hotel_id, h.address;
+ORDER BY total_rooms;
+
+--nested
+--checks if a room in a hotel has a sea view
+SELECT 
+    h.hotel_id,
+    h.address
+FROM hotel h
+WHERE h.hotel_id IN (
+    SELECT hotel_id
+    FROM room
+    WHERE view_type = 'sea_view'
+);
+
+
+
+
+
